94e1ec5958046a1eaa0ac510c41fa111ddc82c35
yogjasonhub <jasonthaoljt@gmail.com>
2023-10-25 21:51:47 -0500

    uupdated repoPoke

--- PokemonReviewApp/Repository/PokemonRepository.cs
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -12,9 +12,38 @@ namespace PokemonReviewApp.Repository
             _context = context;
         }
 
+        public Pokemon GetPokemon(int id)
+        {
+            //throw new NotImplementedException();
+            return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            //throw new NotImplementedException();
+            return _context.Pokemon.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRatings(int pokeId)
+        {
+            //throw new NotImplementedException();
+            var review = _context.Reviews.Where(p => p.Pokemon.Id == pokeId);
+            if (review.Count() <= 0)
+            {
+                return 0;
+            }
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
+        }
+
         public ICollection<Pokemon> GetPokemons() 
         {
             return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokeId)
+        {
+            //throw new NotImplementedException();
+            return _context.Pokemon.Any(p => p.Id == pokeId);
         }
     }
 }
